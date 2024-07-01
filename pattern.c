e5a8384aaa16115e0be033504ab4bb2136a9cead
Ameeraltaf <ameeraltafp04@gmail.com>
2024-07-02 01:00:30 +0530

    patttern 2

--- pattern.c
+++ pattern.c
@@ -1,16 +1,19 @@
-#include<stdio.h>
-int main ()
-{
-  int i,j,row ;
+// 
+
+// next
+
+  #include<stdio.h>
+  int main (){
+    int i,j,row;
   printf("enter the number of rows\n");
   scanf("%d",&row);
-  for (i=1;i<=row;i++)
-   {
+  for(i=row;i>=1;i--)
+  {
     for(j=1;j<=i;j++)
     {
         printf("*");
     }
-        printf("\n");
-   }
-   return 0;
-}
+    printf("\n");
+  }
+    return 0;
+  }
